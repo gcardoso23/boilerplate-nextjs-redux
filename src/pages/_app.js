be0d087755407196516c776a4552e7de270d5e13
Gustavo Cardoso <g.cardoso23@outlook.com>
2021-06-03 19:39:59 -0300

    add styled system theme

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,17 +2,21 @@ import Head from 'next/head';
 import { wrapper } from 'store/store';
 
 import GlobalStyles from 'styles/global';
+import { ThemeProvider } from 'styled-components';
+import theme from 'styles/theme';
 
 function App({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <title>Boilerplate</title>
-        <link rel="shortcut icon" href="/img/favicon.ico" />
-        <meta name="description" content="A simple project starter with Nextjs" />
-      </Head>
-      <GlobalStyles/>
-      <Component {...pageProps} />
+      <ThemeProvider theme={ theme }>
+        <Head>
+          <title>Boilerplate</title>
+          <link rel="shortcut icon" href="/img/favicon.ico" />
+          <meta name="description" content="A simple project starter with Nextjs" />
+        </Head>
+        <GlobalStyles/>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   )
 }
